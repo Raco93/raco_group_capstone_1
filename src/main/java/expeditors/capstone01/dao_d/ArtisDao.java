bd60b035f63f07314a394263e58d6acae355f8b9
sel-davids <david.shin@expeditors.com>
2025-01-15 18:44:16 +0900

    Dao

--- src/main/java/expeditors/capstone01/dao_d/ArtisDao.java
+++ src/main/java/expeditors/capstone01/dao_d/ArtisDao.java
@@ -1,4 +1,39 @@
 package expeditors.capstone01.dao_d;
 
+import expeditors.capstone01.domian_r.Artist;
+import expeditors.capstone01.domian_r.Track;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ArtisDao {
+
+    private final Map<Integer, Artist> artists = new HashMap<>();
+
+    private static final AtomicInteger nextId = new AtomicInteger(1);
+
+    public void addArtist(Artist artist) {
+        artist.setId(nextId.getAndIncrement());
+        artists.put(artist.getId(), artist);
+    }
+
+    public Artist getArtistById(int id) {
+
+        return artists.get(id);
+    }
+
+    public Map<Integer, Artist> getAllArtists() {
+        return artists;
+    }
+
+    public void updateArtist(Artist artist) {
+        if (artists.containsKey(artist.getId())) {
+            artists.put(artist.getId(), artist);
+        }
+    }
+
+    public void deleteArtist(int id) {
+        artists.remove(id);
+    }
 }
--- src/main/java/expeditors/capstone01/dao_d/TrackDao.java
+++ src/main/java/expeditors/capstone01/dao_d/TrackDao.java
@@ -1,4 +1,34 @@
 package expeditors.capstone01.dao_d;
 
+import expeditors.capstone01.domian_r.Artist;
+import expeditors.capstone01.domian_r.Track;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TrackDao {
+
+    private final Map<Integer, Track> tracks = new HashMap<>();
+
+    public void addTrack(Track track) {;
+        tracks.put(track.getId(), track);
+    }
+
+    public Track getTrackById(int id) {
+        return tracks.get(id);
+    }
+
+    public Map<Integer, Track> getAllTracks() {
+        return tracks;
+    }
+
+    public void updateTrack(Track track) {
+        if (tracks.containsKey(track.getId())) {
+            tracks.put(track.getId(), track);
+        }
+    }
+
+    public void deleteTrack(int id) {
+        tracks.remove(id);
+    }
 }
